2b3bdedfdfb0d35459532149a4904d42d4e5359d
zhujianan <2494911084@qq.com>
2020-11-16 16:26:51 +0800

    readme.md文件

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -5,6 +5,7 @@ use Dcat\Admin\Grid;
 use Dcat\Admin\Form;
 use Dcat\Admin\Grid\Filter;
 use Dcat\Admin\Show;
+use Dcat\Admin\Layout\Menu;
 
 /**
  * Dcat-admin - admin builder based on Laravel.
@@ -24,3 +25,27 @@ use Dcat\Admin\Show;
  * Admin::js('/packages/prettydocs/js/main.js');
  *
  */
+
+ ## 动态菜单添加
+Admin::menu(function (Menu $menu) {
+    $menu->add([
+        [
+            'id'            => '1', // 此id只要保证当前的数组中是唯一的即可
+            'title'         => '项目管理',
+            'icon'          => 'fa-chrome',
+            'uri'           => '/project',
+            'parent_id'     => 0,
+            'permission_id' => '', // 与权限绑定
+            'roles'         => '', // 与角色绑定
+        ],
+        [
+            'id'            => '2', // 此id只要保证当前的数组中是唯一的即可
+            'title'         => '文件管理',
+            'icon'          => 'fa-book',
+            'uri'           => '/word',
+            'parent_id'     => 0,
+            'permission_id' => '', // 与权限绑定
+            'roles'         => '', // 与角色绑定
+        ],
+    ]);
+});
